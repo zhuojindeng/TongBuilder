14923298ab3f5bc24d12f3df593566acc947a781
ZhuoJindeng <zhuojin@siweisoft.cn>
2024-05-16 10:04:44 +0800

    DynamicForm

---
+++ TongBuilder/Library/TongBuilder.RazorLib/Models/DataX.cs
@@ -0,0 +1,9 @@
+﻿namespace TongBuilder.RazorLib.Models
+{
+    public class DataX
+    {
+        public string Name { get; set; } = "Jane Doe";
+        public string Phone { get; set; } = "123456789";
+        public string Address { get; set; } = "The Street";
+    }
+}
